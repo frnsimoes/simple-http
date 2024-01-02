037d9d045028e93156f49d68ba8df3c5f1cb0a55
frnsimoes <simoes.grcia@gmail.com>
2024-01-02 00:19:49 -0300

    Change default Content-Type."

--- simple_http/response.py
+++ simple_http/response.py
@@ -2,4 +2,4 @@ class Response:
     def __init__(self, body, status, headers=None):
         self.body = body
         self.status = status
-        self.headers = headers or [("Content-type", "text/plain")]
+        self.headers = headers or [("Content-type", "application/json")]
